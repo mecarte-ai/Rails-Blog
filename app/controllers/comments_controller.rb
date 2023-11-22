1c6c917b0121bf04015a7acbf1b6db5fb7bf18d8
mecarte-ai <93726982+mecarte-ai@users.noreply.github.com>
2023-11-22 20:51:43 +0800

    add comments index

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
 end
